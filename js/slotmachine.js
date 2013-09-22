8af8916da35a1c26a939251b9869c169161e80da
XSilent <michael.neuhaus@gmx.de>
2013-09-22 19:41:24 +0200

    Generic image handling for slotmachines ribbons - and add jQuery lib

--- js/slotmachine.js
+++ js/slotmachine.js
@@ -6,6 +6,8 @@
  */
 var Slotmachine = function()
 {
+	var _ribbonImagesPath;
+	var _ribbonImages = [];
 
 	this.init = function()
 	{
@@ -14,11 +16,58 @@ var Slotmachine = function()
 		var mydiv = $("div:last");
 		var offset = mydiv.offset();
 
-		console.log(offset.top);
+		_initImages();
+		_initRibbons();
 	};
 
+	function _initImages()
+	{
+		_ribbonImagesPath = 'images/items/';
+
+		_ribbonImages.push('0.jpg');
+		_ribbonImages.push('1.jpg');
+		_ribbonImages.push('2.jpg');
+		_ribbonImages.push('3.jpg');
+		_ribbonImages.push('4.jpg');
+		_ribbonImages.push('5.jpg');
+		_ribbonImages.push('6.jpg');
+		_ribbonImages.push('7.jpg');
+		_ribbonImages.push('8.jpg');
+		_ribbonImages.push('9.jpg');
+		_ribbonImages.push('10.jpg');
+		_ribbonImages.push('11.jpg');
+		_ribbonImages.push('12.jpg');
+	}
+
+	function _initRibbons()
+	{
+		for(var i = 1;i <= 3;i++) {
+
+			_shuffle(_ribbonImages);
+
+			for(var j = 0;j < _ribbonImages.length; j++) {
+				var imageElement = $('<img src="' + _ribbonImagesPath + _ribbonImages[j] + '" alt="dingsda">');
+
+ 				$('#slot' + i + ' div.ribbon' ).append(imageElement);
+			}
+		}
+	}
+
+	function _shuffle(arr)
+	{
+		for(var j, x, i = arr.length; i; j = parseInt(Math.random() * i), x = arr[--i], arr[i] = arr[j], arr[j] = x);
+
+		return arr;
+	}
+
+	this.play = function()
+	{
+
+	};
 
 };
 var slotmachine = new Slotmachine();
 
-slotmachine.init();
+$(document).ready(function(){
+	slotmachine.init();
+});
